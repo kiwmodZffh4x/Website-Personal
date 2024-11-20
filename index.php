a95b8a0797001ee322c4e4813aa4fdc175259114
kiwmodZffh4x <modzmasky@gmail.com>
2024-11-20 11:49:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Kalkulator</title>
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+	<link rel="stylesheet" type="text/css" href="assets/style/style.css">
+</head>
+<body>
+	<div class="contaiiner-fluid bg-img d-flex justify-content-center align-items-center">
+		<div class="card p-5 bg-glases border border-light text-white">
+			
+		</div>
+	</div>
+
+</body>
+</html>
